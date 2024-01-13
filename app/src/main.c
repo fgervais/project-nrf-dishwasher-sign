29a30379b2012f25638c238268dfaa9466232a58
Francois Gervais <francoisgervais@gmail.com>
2024-01-13 09:10:37 -0500

    Revert "template: remove the button handling"
    This reverts commit aceaf0a188b705bca17bffc92bbe951c874cb53c.

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/pm/device.h>
 
+#include <app_event_manager.h>
+
+#define MODULE main
+#include <caf/events/module_state_event.h>
+#include <caf/events/button_event.h>
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
@@ -14,6 +20,12 @@ int main(void)
 	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 #endif
 
+	if (app_event_manager_init()) {
+		LOG_ERR("Event manager not initialized");
+	} else {
+		module_set_state(MODULE_STATE_READY);
+	}
+
 	LOG_INF("\n\n🚀 MAIN START (%s) 🚀\n", APP_VERSION_FULL);
 
 	LOG_INF("🎉 init done 🎉");
@@ -24,3 +36,23 @@ int main(void)
 
 	return 0;
 }
+
+static bool event_handler(const struct app_event_header *eh)
+{
+	int ret;
+	const struct button_event *evt;
+
+	if (is_button_event(eh)) {
+		evt = cast_button_event(eh);
+
+		if (evt->pressed) {
+			LOG_INF("Pin Toggle");
+			ret = gpio_pin_toggle_dt(&led);
+		}
+	}
+
+	return true;
+}
+
+APP_EVENT_LISTENER(MODULE, event_handler);
+APP_EVENT_SUBSCRIBE(MODULE, button_event);
